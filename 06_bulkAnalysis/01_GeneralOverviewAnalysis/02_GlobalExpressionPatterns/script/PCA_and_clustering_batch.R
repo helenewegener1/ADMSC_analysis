e28dc6cb3cfd61a336cbbac5b726cac88b025410
helenewegener1 <helene.wegener@gmail.com>
2026-04-23 09:31:23 +0200

    adding it loadings

--- 06_bulkAnalysis/01_GeneralOverviewAnalysis/02_GlobalExpressionPatterns/script/PCA_and_clustering_batch.R
+++ 06_bulkAnalysis/01_GeneralOverviewAnalysis/02_GlobalExpressionPatterns/script/PCA_and_clustering_batch.R
@@ -14,10 +14,11 @@ library(ggplot2)
 library(glue)
 library(pheatmap)
 library(RColorBrewer)
+library(writexl)
 # detach("package:biomaRt", unload = TRUE, character.only = TRUE)
 
-# batch_nr <- 1
-batch_nr <- 2
+batch_nr <- 1
+# batch_nr <- 2
 
 # ################################## Meta data ################################### 
 # # Load and wrangle metadata
@@ -132,6 +133,68 @@ for (var in colnames(metadata_clean)){
     
   }
 }
+
+################################# PCA loadings ################################# 
+# Run PCA manually using prcomp on the VST counts
+# (plotPCA internally does this, but we need the rotation matrix)
+
+# Get the VST matrix (genes x samples)
+vst_mat <- assay(vsd)
+
+# Run PCA (transposed: samples x genes, as prcomp expects observations x variables)
+pca_res <- prcomp(t(vst_mat), scale. = FALSE, center = TRUE)
+
+# Number of PCs you want to extract gene weights for
+n_pcs <- 3  # adjust as needed (covers PC1, PC2, PC3 from your combinations)
+
+# Connect to Ensembl
+library(biomaRt)
+mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+# Extract top 50 up and 50 down genes for each PC
+top_genes_per_pc_list <- lapply(1:n_pcs, function(pc_i) {
+  
+  # Rotation/loadings for this PC (named vector, one value per gene)
+  loadings <- pca_res$rotation[, pc_i]
+  
+  # Sort descending
+  sorted_loadings <- sort(loadings, decreasing = TRUE)
+  
+  # Top 50 positive (up) and top 50 negative (down)
+  top50_up   <- head(sorted_loadings, 100)
+  top50_down <- tail(sorted_loadings, 100)
+  
+  df <- data.frame(
+    gene     = c(names(top50_up), names(top50_down)) %>% str_split_i('\\.', 1),
+    loading  = c(top50_up, top50_down),
+    direction = c(rep("up", 100), rep("down", 100)),
+    PC       = rep(glue("PC{pc_i}"), 200)
+  )
+  
+  # Get mapping
+  gene_map <- getBM(
+    attributes = c("ensembl_gene_id", "hgnc_symbol"),
+    filters = "ensembl_gene_id",
+    values = df$gene, #%>% str_split_i('\\.', 1),
+    mart = mart
+  )
+  
+  gene_map <- gene_map %>% 
+    mutate(hgnc_symbol = case_when(hgnc_symbol == "" ~ ensembl_gene_id, .default = hgnc_symbol)) %>% 
+    dplyr::rename(gene = "ensembl_gene_id")
+  
+  df <- df %>% left_join(gene_map, by = "gene") %>% relocate(hgnc_symbol, .after = gene)
+  
+  return(df)
+  
+}) %>% setNames(glue("PC{1:3}"))
+
+# Save to excel
+write_xlsx(
+  top_genes_per_pc_list,
+  path = glue("06_bulkAnalysis/01_GeneralOverviewAnalysis/02_GlobalExpressionPatterns/table/batch_{batch_nr}_top_genes_per_PC.xlsx")
+)
+
 
 ################################################################################
 ###################### Hierarchical clustering/dendrogram ###################### 
